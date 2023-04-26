dcafd5466fb8aa6f5955fc8e6c177f702fcab1a1
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-04-26 17:22:59 -0500

    feat: :sparkles: It Works!
    The main functions required by the project are finished, the styles are adjusted a little more, the table and modals are correctly injected with the API Data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let count = 0
 const tbody =document.querySelector("tbody")
 const url = "https://vermenmasterchief.tk/estudiantes.php"
-const url2 = `https://vermenmasterchief.tk/detalleEstudiante.php?api_key=Metallica&id_usuario=`
+
 async function asincronica(){
 /*     fetch(url) */
 /*     .then(response => response.json()) */
@@ -12,12 +12,28 @@ const result = await response.json()
     insertTable(result.datos)
 }
 
-function insertTable(estudiantes){
+async function insertTable(estudiantes){
     count = -1
     for(let i = 0; i < estudiantes.length; i++) {
+        let id= estudiantes[i].id_usuario;
+        let datosModal=[]
         count++;
+        let url2 = `https://vermenmasterchief.tk/detalleEstudiante.php?api_key=Metallica&id_usuario=${id}`
+        const response = await fetch(url2)
+        const result = await response.json()
+        datosModal=result.datos;
+        let aprobado
+        let clase
+        if(Number(datosModal[0].promedio)<3)
+        {
+            aprobado="Reprobado"
+            clase="badge bg-danger";
+        }
+        else{
 
-        
+            aprobado="Aprobado"
+            clase="badge bg-success"
+        }
         tbody.insertAdjacentHTML("beforeend", `
     <tr>
         <th>${estudiantes[i].cedula}</th>
@@ -25,19 +41,27 @@ function insertTable(estudiantes){
         <th>${estudiantes[i].programa}</th>
         <th>${estudiantes[i].sexo}</th>
         <th>${estudiantes[i].jornada}</th>
-        <th><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#miModal">Details</button></th>
+        <th><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#miModal${i}">Details</button></th>
     </tr>
-        <div class="modal fade" id="miModal" tabindex="-1" aria-hidden="true" aria-labelledby="modalTitle">
-            <div class="modal-dialog">
+        <div class="modal fade " id="miModal${i}" tabindex="-1" aria-hidden="true" aria-labelledby="modalTitle">
+            <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-tittle" id="modalTitle">Information </h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary"data-bs-dismiss="modal"> Cerrar</button>
+                        <img src="${datosModal[0].foto}">
+                        <div class="modal-body-text">
+                            <h4>${estudiantes[i].nombre}</h4>
+                            <h5>promedio: ${datosModal[0].promedio}</h5>
+                            sisben: ${datosModal[0].sisben}
+                            <h3><span class="${clase}">${aprobado}</span></h3>
+                            
                         </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary"data-bs-dismiss="modal"> Cerrar</button>
                     </div>
                 </div>
             </div>
