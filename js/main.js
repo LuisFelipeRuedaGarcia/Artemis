7fa00387a145c228443ba7b5f4aa190344e5033a
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-04-28 09:24:07 -0500

    Feat: :sparkles: Initial Commit The project starts, exploring the API...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 //
+const fromatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0
+})
+const btccop = async () => {
+    const url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=COP';
+    const respuesta = await fetch(url);
+    let resultado = await respuesta.json();
+    console.log(resultado.DISPLAY['BTC']
+    ['COP'])
+    resultado = resultado.DISPLAY['BTC']
+    ['COP']
+    console.log(resultado)
+    if(Object.keys(resultado).length === 0) {return null;}
+    let preciobtc= resultado.PRICE;
+    console.log(preciobtc)
+     preciobtc=parseFloat(preciobtc.replace(/[COP,]/g, ""));
+     console.log(preciobtc)
+     
+}
+btccop()
