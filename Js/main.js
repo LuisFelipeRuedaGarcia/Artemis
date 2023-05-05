6146ed8082f17e21ea2a3b361a92e01eaf4b9926
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-05-05 09:28:43 -0500

    version 1

--- Js/main.js
+++ Js/main.js
@@ -1,50 +1,92 @@
 let tbodyClientes = document.querySelector("#tbodyClientes");
 let form = document.querySelector("#NuevoCliente");
-console.log(tbodyClientes);
+let url = "http://localhost:4000/clientes";
+
 (async function getClientes(){
-    let url = "http://localhost:4002/clientes";
     let response = await fetch(url);
     let  result= await response.json();
     insertClientes(result);
 })();
 
-/* {
-    "nombre": "Juan",
-    "apellido": "Pérez",
-    "telefono": "555-1234",
-    "correo": "juan@example.com",
-    "PropietarioId": 1,
-    "mascotas": [
-      {
-        "id": 1,
-        "nombre": "Luna",
-        "tipo": "Perro",
-        "raza": "Labrador",
-        "edad": 3,
-        "notas": "Luna es una perra muy juguetona y le encanta correr en el parque."
-      }
-    ]
-  }, */
-
 function insertClientes(clientes){
     clientes.forEach(cliente => {
     tbodyClientes.insertAdjacentHTML("beforeend", `<tr>
-    <td>${cliente.nombre, cliente.apellido}</td>
+    <td>${cliente.nombre} ${cliente.apellido}</td>
     <td>
     teléfono: ${cliente.telefono}
     <br>
     correo: ${cliente.correo}
     </td>
     <td>${cliente.mascotas[0].nombre}</td>
-    <td><button type="button" id=${cliente.PropietarioId} database-cliente="${cliente.PropietarioId}>Eliminar</button></td>
+    <td><button type="button" id=btn${cliente.id} data-cliente="${cliente.id}">Eliminar</button></td>
     </tr>`);
+eventlistners(cliente.id);
 });
+console.log("cuando");
 }
 
 form.addEventListener("submit", (e)=>{
-    validateCliente(e);
+  formDataCliente(e);
 })
-function validateCliente(e){
-    e.preventDefault();
+function formDataCliente(e){
+  e.preventDefault();
+  let  dataform = Object.fromEntries(new FormData(e.target));
+  let newCliente = {
+    nombre: dataform.nombre,
+    apellido: dataform.apellido,
+    telefono: dataform.telefono,
+    correo: dataform.correo,
+    PropietarioId: dataform.PropietarioId,
+    mascotas: [
+      {
+        id: dataform.id,
+        nombre: dataform.nombreM,
+        tipo: dataform.tipo,
+        raza: dataform.raza,
+        edad: dataform.edad,
+        notas: dataform.notas
+      }]
+  }
+  postCliente(newCliente);
+}
+
+async function postCliente(object){
+
+  try {
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(object),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  } 
+}
+
+
+async function deleteCliente(id){
+  try{
+    await fetch(`${url}/${id}`,{
+      method: "DELETE",
+    })
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+function eventlistners(id) {
+  let button = document.querySelector(`#btn${id}`);
+  console.log(button);
+ button.addEventListener("click", idParaBorrar)
 
 }
+
+function idParaBorrar(e){
+  let dataset = Number(e.target.dataset.cliente);
+/*   let id  */
+  console.log(dataset);
+}
+
